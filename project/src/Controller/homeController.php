74e59d0a010b95cccc13c060ffcb34c055100fa9
Slvn89 <sylvain.dumaine@hotmail.fr>
2023-11-14 09:06:44 +0100

    initial commit 'hello'

---
+++ project/src/Controller/homeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class homeController extends AbstractController
+{
+    #[Route('/', name: 'app.home')]
+    public function home(): Response
+    {
+       return $this->render('pages/home.html.twig');
+    }
+}
+
